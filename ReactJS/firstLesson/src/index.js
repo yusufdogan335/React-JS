9258f11f8512830c181225d74a37b2f8ae1c1ecc
yusufdogan335 <yusufdogann@hotmail.com>
2021-08-07 00:23:07 +0300

    added files

---
+++ ReactJS/firstLesson/src/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from './components/Card';
+import Collapse from './components/Collapse';
+
+
+const App = () => {
+    return (
+        <div className="card-group">
+            <Collapse href="collapseExample1">
+                <Card
+                    cardTitle="Card Title 1"
+                    cardText="Lorem Ipsum Text 1"
+                    updatedTime="Last Updated 1 min ago"
+                    image="https://picsum.photos/id/1/200/300"
+                />
+            </Collapse>
+
+
+            <Collapse href="collapseExample2">
+                <Card
+                    cardTitle="Card Title 2"
+                    cardText="Lorem Ipsum Text 2"
+                    updatedTime="Last Updated 2 min ago"
+                    image="https://picsum.photos/id/10/200/300"
+                />
+            </Collapse>
+
+            <Collapse href="collapseExample3">
+                <Card
+                    cardTitle="Card Title 3"
+                    cardText="Lorem Ipsum Text 3"
+                    updatedTime="Last Updated 3 min ago"
+                    image="https://picsum.photos/id/100/200/300"
+                />
+            </Collapse>
+        </div>
+    )
+};
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
+
+
