c25a4aad764d1f071e6f33d3b353bf5e33fdc606
yusufdogan335 <yusufdogann@hotmail.com>
2021-08-07 02:14:48 +0300

    new codes added

--- ReactJS/firstLesson/src/components/Card.js
+++ ReactJS/firstLesson/src/components/Card.js
@@ -1,19 +1,22 @@
 import React from 'react';
 
-const Card = (props) => {
-    return (
-        <div>
-            <div className="card">
-                <img src={props.image} className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">{props.cardTitle}</h5>
-                    <p className="card-text">{props.cardText}</p>
-                    <p className="card-text"><small className="text-muted">{props.updatedTime}</small></p>
+class Card extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="card w-100">
+                    <img src={this.props.image} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{this.props.cardTitle}</h5>
+                        <p className="card-text">{this.props.cardText}</p>
+                        <p className="card-text"><small className="text-muted">{this.props.updatedTime}</small></p>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
+
 
 export default Card;
 
--- ReactJS/firstLesson/src/index.js
+++ ReactJS/firstLesson/src/index.js
@@ -6,34 +6,42 @@ import Collapse from './components/Collapse';
 
 const App = () => {
     return (
-        <div className="card-group">
-            <Collapse href="collapseExample1">
-                <Card
-                    cardTitle="Card Title 1"
-                    cardText="Lorem Ipsum Text 1"
-                    updatedTime="Last Updated 1 min ago"
-                    image="https://picsum.photos/id/1/200/300"
-                />
-            </Collapse>
+        <div className="container">
 
-
-            <Collapse href="collapseExample2">
-                <Card
-                    cardTitle="Card Title 2"
-                    cardText="Lorem Ipsum Text 2"
-                    updatedTime="Last Updated 2 min ago"
-                    image="https://picsum.photos/id/10/200/300"
-                />
-            </Collapse>
-
-            <Collapse href="collapseExample3">
-                <Card
-                    cardTitle="Card Title 3"
-                    cardText="Lorem Ipsum Text 3"
-                    updatedTime="Last Updated 3 min ago"
-                    image="https://picsum.photos/id/100/200/300"
-                />
-            </Collapse>
+            <div className="row">
+                <div className="card-group w-100">
+                    <div className="col">
+                        <Collapse href="collapseExample1">
+                            <Card
+                                cardTitle="Card Title 1"
+                                cardText="Lorem Ipsum Text 1"
+                                updatedTime="Last Updated 1 min ago"
+                                image="https://picsum.photos/id/1/200/300"
+                            />
+                        </Collapse>
+                    </div>
+                    <div className="col">
+                        <Collapse href="collapseExample2">
+                            <Card
+                                cardTitle="Card Title 2"
+                                cardText="Lorem Ipsum Text 2"
+                                updatedTime="Last Updated 2 min ago"
+                                image="https://picsum.photos/id/2/200/300"
+                            />
+                        </Collapse>
+                    </div>
+                    <div className="col">
+                        <Collapse href="collapseExample3">
+                            <Card
+                                cardTitle="Card Title 3"
+                                cardText="Lorem Ipsum Text 3"
+                                updatedTime="Last Updated 3 min ago"
+                                image="https://picsum.photos/id/3/200/300"
+                            />
+                        </Collapse>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
