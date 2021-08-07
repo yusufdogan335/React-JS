8f58d6e4350839dc18089653315b582febd9d92f
yusufdogan335 <yusufdogann@hotmail.com>
2021-08-08 01:28:38 +0300

    setState worked

--- ReactJS/firstLesson/src/components/Collapse.js
+++ ReactJS/firstLesson/src/components/Collapse.js
@@ -1,3 +1,9 @@
+// first constructor -->state initialization,
+// render first worked,
+// onClick event worked,
+// setState --> triggered render method,
+// render method shows current JSX on page.
+
 import React from 'react';
 
 class Collapse extends React.Component {
@@ -6,18 +12,37 @@ class Collapse extends React.Component {
         super();
 
         this.state = {
-            showContent: true
+            showContent: false
         }
 
+        /* this.showMore = () => {
+            console.log(this)
+        } */
+
+        //this.showMore = this.showMore.bind(this)
 
     }
+
+    /* showMore() {
+        this.setState({ showContent: true })
+        console.log(this)
+    } */
+
+    showMore = () => {
+        this.setState({ showContent: !this.state.showContent })
+
+        /* this.state = {
+            showContent: true
+        } */
+    }
+
 
     render() {
         return (
             <div>
-                <a className="btn btn-primary w-100">
+                <button className="btn btn-primary w-100" onClick={this.showMore}>
                     Link with href
-                </a>
+                </button>
 
                 {
                     this.state.showContent ? (
