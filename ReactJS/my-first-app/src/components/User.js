09953b5151d041743c0bc5941dd8c453bd63fd05
yusufdogan335 <yusufdogann@hotmail.com>
2021-08-14 03:11:57 +0300

    bind methode worked

--- ReactJS/my-first-app/src/components/User.js
+++ ReactJS/my-first-app/src/components/User.js
@@ -3,31 +3,59 @@ import PropTypes from 'prop-types';
 
 class User extends Component {
 
-    /*     static defaultProps = {
-            name: "Bilgi Yok",
-            salary: "Bilgi Yok",
-            department: "Bilgi Yok"
+    state = {
+        isVisible: false
+    }
+
+    static defaultProps = {
+        name: "Bilgi Yok",
+        salary: "Bilgi Yok",
+        department: "Bilgi Yok"
+    }
+
+    /*     constructor(props) {
+            super(props);
+    
+            this.state = {
+                isVisible: false
+            }
         } */
+
+    /*     This Bind Methode 1:
+            constructor(props) {
+                super(props)
+                this.onClickEvent = this.onClickEvent.bind(this)
+            } */
+
+
+
+    onClickEvent = (number, e) => {
+        console.log(number)
+    }
+
 
     render() {
 
         //Destructing
-        const { name, department, salary } = this.props;
+        const { name, department, salary, } = this.props;
+        const { isVisible } = this.state;
 
         return (
             <div className="col-md-8 mb-4">
                 <div className="card">
                     <div className="card-header d-flex justify-content-between">
-                        <h4 className="d-inline">{name}</h4>
+                        <h4 className="d-inline" onClick={this.onClickEvent.bind(this, 34)}>{name}</h4>
                         <i className="far fa-trash-alt" style={{ cursor: "pointer" }}></i>
                     </div>
+                    {
+                        isVisible ? <div className="card-body">
+                            <p className="card-text"> Salary : {salary}</p>
+                            <p className="card-text"> Department : {department}</p>
+                        </div> : null
+                    }
 
-                    <div className="card-body">
-                        <p className="card-text"> Salary : {salary}</p>
-                        <p className="card-text"> Department : {department}</p>
-                    </div>
                 </div>
-            </div>
+            </div >
         )
     }
 }
