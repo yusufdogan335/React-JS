f78e6b1c2f9ab71c581d822f6c61d28742b9df05
yusufdogan335 <yusufdogann@hotmail.com>
2021-08-19 01:42:57 +0300

    Provider - Consumer used

--- ReactJS/books/src/components/App.js
+++ ReactJS/books/src/components/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import BookList from './BookList';
+import BookContextProvider from '../contexts/BookContext';
 
 
 class App extends React.Component {
 
     render() {
         return (
-            <div> <BookList /></div>
+            <div>
+                <BookContextProvider>
+                    <BookList />
+                </BookContextProvider>
+            </div>
         )
 
     }
